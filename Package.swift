9e59f0d44df2c0b3fb8763d93c0f45be1baed474
Patrick Tran <patrick.tran06@gmail.com>
2021-03-02 09:05:37 -0800

    copying initial plugin code from docs

--- Package.swift
+++ Package.swift
@@ -8,13 +8,27 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .macOS(.v10_15),
+    ],
+    products: [
+        .executable(name: "apikey_xcmetrics", targets: ["apikey_xcmetrics_app"]),
+    ],
+    dependencies: [
+         .package(url: "https://github.com/spotify/XCMetrics", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "apikey_xcmetrics",
-            dependencies: []),
+            dependencies: [
+                .product(name: "XCMetricsClient", package: "XCMetrics"),
+                .product(name: "XCMetricsPlugins", package: "XCMetrics")
+            ]
+        ),
+        .target(
+            name: "apikey_xcmetrics_app",
+            dependencies: ["apikey_xcmetrics"]
         .testTarget(
             name: "apikey_xcmetricsTests",
             dependencies: ["apikey_xcmetrics"]),
--- Sources/apikey_xcmetrics/main.swift
+++ Sources/apikey_xcmetrics/main.swift
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import Foundation
+import XCMetricsClient
+import XCMetricsPlugins
+
+public struct apikey_xcmetrics {
+
+    public static func main() {
+        let metrics = XCMetrics.parseOrExit()
+        let configuration = XCMetricsConfiguration()
+        configuration.add(plugin: ThermalThrottlingPlugin().create()) // ThermalThrottlingPlugin lives in XCMetricsPlugins.
+        metrics.run(with: configuration)
+    }
+}
